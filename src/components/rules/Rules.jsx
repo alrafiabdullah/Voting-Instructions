ee4edfca111e87a7978be188a32c192b61632f2d
abdullah-al-rafi <abdullah34alrafi@gmail.com>
2020-12-26 11:14:49 +0600

    rules added

--- src/components/rules/Rules.jsx
+++ src/components/rules/Rules.jsx
@@ -3,45 +3,44 @@ import React, { Component } from "react";
 import "./rules.css";
 
 export class Rules extends Component {
+  /*
+
+  Do not share the voting form link with anyone else. If someone tells you they did not get the form, tell them to contact the EBs.
+
+You are not allowed to cast a vote on anyone else's behalf. You can only cast your own vote. If you are found to be voting in someone else's name, action will be taken.
+
+You may only vote during the election period. No more votes will be accepted once the period is over.
+
+You may only vote once. You cannot edit your vote after it has been cast.
+
+If you do not follow the rules and instructions properly, your vote may be discarded.
+
+  */
+
   render() {
     return (
       <div>
         <ul className="list-group list-group-fllush">
           <li className="list-group-item list-group-item-primary">
-            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Illum,
-            molestiae!
+            Do not share the voting form link with anyone else. If someone tells
+            you they did not get the form, tell them to contact the EBs.
           </li>
           <li className="list-group-item list-group-item-primary">
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsa,
-            veniam?
+            You are not allowed to cast a vote on anyone else's behalf. You can
+            only cast your own vote. If you are found to be voting in someone
+            else's name, action will be taken.
           </li>
           <li className="list-group-item list-group-item-primary">
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsa,
-            veniam?
+            You may only vote during the election period. No more votes will be
+            accepted once the period is over.
           </li>
           <li className="list-group-item list-group-item-primary">
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsa,
-            veniam?
+            You may only vote once. You cannot edit your vote after it has been
+            cast.
           </li>
           <li className="list-group-item list-group-item-primary">
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsa,
-            veniam?
-          </li>
-          <li className="list-group-item list-group-item-primary">
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsa,
-            veniam?
-          </li>
-          <li className="list-group-item list-group-item-primary">
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsa,
-            veniam?
-          </li>
-          <li className="list-group-item list-group-item-primary">
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsa,
-            veniam?
-          </li>
-          <li className="list-group-item list-group-item-primary">
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsa,
-            veniam?
+            If you do not follow the rules and instructions properly, your vote
+            may be discarded.
           </li>
         </ul>
         <div className="confirmation alert alert-success">
@@ -58,7 +57,7 @@ export class Rules extends Component {
         </div>
         <div className="alert alert-info confirm">
           <a
-            href="https://voteacm.herokuapp.com/vote/1"
+            href="https://forms.gle/RPSZcVVGXgtTRJuj8"
             className="btn btn-info mt-3"
             style={{ marginRight: "40%", marginLeft: "40%" }}
           >
